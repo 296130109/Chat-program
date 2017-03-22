9e462928eb70f3dad34ec3dbb4a04c6d0ae00053
296130109 <296130109@qq.com>
2017-03-22 14:03:22 +0800

    base activity

---
+++ app/src/main/java/com/example/chat_program/act/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.chat_program.act;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 阳 on 2017/3/22.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}

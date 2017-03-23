997e92f84a6ea2f29915075d6fca8f4c790ab35e
296130109 <296130109@qq.com>
2017-03-23 10:51:52 +0800

    new por

--- app/src/main/java/com/example/chat_program/act/BaseActivity.java
+++ app/src/main/java/com/example/chat_program/act/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.chat_program.act;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
-
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
